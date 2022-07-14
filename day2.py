bce478527831bb5c0afe119ef659c7aedbbb28ac
stefano-faso <109248999+stefano-faso@users.noreply.github.com>
2022-07-13 21:56:39 -0400

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,16 +1,29 @@
+with open('input2') as txt:
+    data = [(command,int(value)) for command, value in (line.split() for line in txt)]
+    
 #--- day2 part 1 ---#
 
-with open('input2') as txt:
-    data = [(command,int(value)) for command, value in(line.split() for line in txt)]
+def part1():
+    horizontal,depth= 0,0
 
-horizontal,depth = 0,0
+    for command,value in data:
+        if command == 'forward':
+            horizontal += value
+        elif command == 'down':
+            depth += value
+        else:depth -= value
+    print(horizontal*horizontal)
 
-for command,value in data:
-    if command == 'forward':
-        horizontal += value
-    elif command == 'down':
-        depth += value
-    else:
-        depth -= value
-position = horizontal * depth
-print(horizontal,depth, position)
+#--- day2 part 2 ---#
+
+def part2():
+    depth,horizontal,aim =0,0,0
+
+    for command, value in data:
+        if command == 'forward':
+            horizontal += value
+            depth += value * aim
+        elif command == 'down':
+            aim += value
+        else:aim -= value
+    print(depth * horizontal)
